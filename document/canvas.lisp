f62ad4d0698d4ee636b8783d2371869e829353b5
Shinmera <shinmera@tymoon.eu>
2014-06-09 00:24:50 +0200

    Moved stuff to layers (primitive)

--- document/canvas.lisp
+++ document/canvas.lisp
@@ -8,15 +8,7 @@
 (named-readtables:in-readtable :qt)
 
 (defclass canvas ()
-  (;; Properties
-   (%width :initform 500 :initarg :width :accessor width)
-   (%height :initform 500 :initarg :height :accessor height)
-   (%bg-brush :initform NIL :initarg :bg-brush :accessor bg-brush)
-   ;; Qt instances
-   (%brush :initform (#_new QBrush) :accessor brush)
-   (%pen :initform (#_new QPen) :accessor pen)
-   (%pixmap :initform NIL :accessor pixmap)
-   (%painter :initform NIL :accessor painter)
+  ((%bg-brush :initform NIL :initarg :bg-brush :accessor bg-brush)
    ;;
    (%document :initarg :document :initform (error "Document required.") :accessor document)
    (%active-layer :initform 0 :accessor active-layer)
@@ -24,72 +16,35 @@
    (%stroke :initform NIL :accessor stroke)))
 
 (defmethod initialize-instance :after ((canvas canvas) &key)
-  (#_setColor (brush canvas) (#_new QColor 0 0 0))
-  (#_setColor (pen canvas) (#_new QColor 0 0 0))
-  (#_setStyle (brush canvas) (#_Qt::SolidPattern))
-  (#_setWidthF (pen canvas) 1)
-  (#_setCapStyle (pen canvas) (#_Qt::RoundCap))
-  (#_setJoinStyle (pen canvas) (#_Qt::RoundJoin))
   (setf (background canvas) (merge-pathnames "background.png" (asdf:system-source-directory :parasol)))
-  (add-layer canvas)
-  (resize-canvas canvas (width canvas) (height canvas)))
+  (add-layer canvas))
 
 (defgeneric resize-canvas (canvas width height &optional x-offset y-offset)
   (:method ((canvas canvas) width height &optional (x-offset 0) (y-offset 0))
-    (let ((pixmap (#_new QPixmap width height)))
-      (#_fill pixmap (#_new QColor 0 0 0 0))
-      (when (pixmap canvas)
-        (#_end (painter canvas))
-        (let ((painter (#_new QPainter pixmap)))
-          (#_drawPixmap painter x-offset y-offset (pixmap canvas))
-          (optimized-delete (pixmap canvas))
-          (#_end painter)))
-      (let ((painter (#_new QPainter pixmap)))
-        (#_setRenderHint painter (#_QPainter::Antialiasing))
-        (#_setRenderHint painter (#_QPainter::HighQualityAntialiasing))
-        (#_setPen painter (pen canvas))
-        (#_setBrush painter (brush canvas))
-        
-        (setf (pixmap canvas) pixmap
-              (painter canvas) painter
-              (width canvas) width
-              (height canvas) height)))
-    canvas))
+    ))
 
 (defgeneric scale-canvas (canvas width height)
   (:method ((canvas canvas) width height)
-    ;; Dispose of old pixmap
-    (let ((pixmap (#_scaled (pixmap canvas) width height (#_Qt::IgnoreAspectRatio) (#_Qt::SmoothTransformation))))
-      (let ((painter (#_new QPainter pixmap)))
-        (#_setRenderHint painter (#_QPainter::Antialiasing))
-        (#_setRenderHint painter (#_QPainter::HighQualityAntialiasing))
-        (#_setPen painter (pen canvas))
-        (#_setBrush painter (brush canvas))
-        
-        (setf (pixmap canvas) pixmap
-              (painter canvas) painter
-              (width canvas) width
-              (height canvas) height)))))
+    ))
 
 (defgeneric save-canvas (canvas path format)
   (:method ((canvas canvas) (path pathname) format)
-    (#_save (pixmap canvas) (uiop:native-namestring path) format 100)
-    path))
+    ))
 
-(defgeneric start-stroke (canvas type x y x-tilt y-tilt pressure)
-  (:method ((canvas canvas) type x y x-tilt y-tilt pressure)
-    (setf (stroke canvas) (make-instance 'stroke))
-    (record-point canvas x y x-tilt y-tilt pressure)
-    canvas))
+(defmethod start-stroke ((canvas canvas) type x y x-tilt y-tilt pressure)
+  (start-stroke (aref (layers canvas) (active-layer canvas))
+                type x y x-tilt y-tilt pressure)
+  canvas)
 
 (defmethod record-point ((canvas canvas) x y x-tilt y-tilt pressure)
-  (record-point (stroke canvas) x y x-tilt y-tilt pressure)
-  (draw-incremental (stroke canvas) (painter canvas))
+  (record-point (aref (layers canvas) (active-layer canvas))
+                x y x-tilt y-tilt pressure)
   canvas)
 
 (defmethod draw ((canvas canvas) painter)
   (#_fillRect painter (#_rect (document canvas)) (bg-brush canvas))
-  (#_drawPixmap painter 0 0 (pixmap canvas)))
+  (loop for layer across (layers canvas)
+        do (draw layer painter)))
 
 (defun remove-canvas-background (canvas)
   (let ((bg-brush (bg-brush canvas)))
@@ -118,7 +73,8 @@
 (defmethod add-layer ((canvas canvas) &key name mode)
   (let ((layer (make-instance 'layer :name (or name (format NIL "Layer ~d" (length (layers canvas)))) :mode mode)))
     (vector-push-extend layer (layers canvas))
-    layer))
+    layer)
+  (#_update (document canvas)))
 
 (defmethod remove-layer ((canvas canvas) &optional index)
   (when index
@@ -148,7 +104,8 @@
                    (aref layers (1- i))))
     ;; Push in
     (setf (aref layers index) layer
-          (active-layer canvas) index)))
+          (active-layer canvas) index))
+  (#_update (document canvas)))
 
 (defmethod finalize ((canvas canvas))
   (remove-canvas-background canvas)
--- document/layer.lisp
+++ document/layer.lisp
@@ -9,7 +9,8 @@
 
 (defclass layer ()
   ((%pixmap :accessor pixmap)
-   (%strokes :initform () :accessor strokes)
+   (%painter :accessor painter)
+   (%stroke :initform () :accessor stroke)
    (%mode :initarg :mode :accessor mode)
    (%name :initarg :name :accessor name)))
 
@@ -17,3 +18,30 @@
   (print-unreadable-object (layer stream :type T :identity T)
     (format stream "~a" (name layer)))
   layer)
+
+(defmethod initialize-instance :after ((layer layer) &key)
+  (let* ((pixmap (#_new QImage 500 500 (#_QImage::Format_ARGB32_Premultiplied)))
+         (painter (#_new QPainter pixmap)))
+    (#_setRenderHint painter (#_QPainter::Antialiasing))
+    (#_setRenderHint painter (#_QPainter::HighQualityAntialiasing))
+    (#_setStyle (#_brush painter) (#_Qt::SolidPattern))
+    (setf (pixmap layer) pixmap
+          (painter layer) painter)))
+
+(defmethod start-stroke ((layer layer) type x y x-tilt y-tilt pressure)
+  (setf (stroke layer) (make-instance 'stroke))
+  (record-point layer x y x-tilt y-tilt pressure)
+  layer)
+
+(defmethod record-point ((layer layer) x y x-tilt y-tilt pressure)
+  (record-point (stroke layer) x y x-tilt y-tilt pressure)
+  (draw-incremental (stroke layer) (painter layer))
+  layer)
+
+(defmethod draw ((layer layer) painter)
+  (#_drawImage painter 0 0 (pixmap layer)))
+
+(defmethod finalize ((layer layer))
+  (optimized-delete (painter layer))
+  (optimized-delete (pixmap layer))
+  (finalize (stroke layer)))
