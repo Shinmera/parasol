cd22e74f077dc5c47a40c6f05f1ab08a1caf026c
Shinmera <shinmera@tymoon.eu>
2014-06-13 21:34:52 +0200

    Added brush selector to brush-widget

--- gui/brush-widget.lisp
+++ gui/brush-widget.lisp
@@ -11,22 +11,41 @@
   (make-instance 'ex-slider-widget :max 5000 :default 500 :on-change #'brush-widget-update))
 
 (defclass brush-widget ()
-  ((%size :accessor size))
+  ((%size :accessor size)
+   (%brush :accessor brush))
   (:metaclass qt-class)
-  (:qt-superclass "QWidget"))
+  (:qt-superclass "QWidget")
+  (:slots ("setBrush(const QString)" brush-widget-set)))
 
 (defmethod initialize-instance :after ((widget brush-widget) &key)
   (new widget)
   (let ((layout (#_new QGridLayout))
+        (brush (#_new QComboBox))
         (size (%make-size-slider)))
+    (setf (size widget) size
+          (brush widget) brush)
+    (brush-widget-update-choices widget)
     (#_addWidget layout (#_new QLabel "Brush") 0 0)
     (#_addWidget layout size 1 0)
+    (#_addWidget layout brush 2 0)
     (#_setLayout widget layout)
-    (setf (size widget) size)))
+    (connect brush "currentIndexChanged(const QString)" widget "setBrush(const QString)")))
+
+(defun brush-widget-update-choices (widget)
+  (#_clear (brush widget))
+  (loop for class in (closer-mop:class-direct-subclasses (find-class 'brush))
+        do (#_addItem (brush widget) (string-downcase (class-name class)))))
 
 (defun brush-widget-update (size)
   (setf (base-size (current-brush *window*)) (/ size 100)))
 
+(defun brush-widget-set (widget name)
+  (let ((class-name (find-symbol (string-upcase name) "PARASOL")))
+    (when class-name
+      (setf (current-brush *window*) (make-instance class-name)))))
+
 (defmethod finalize ((widget brush-widget))
   (finalize (size widget))
-  (setf (size widget) NIL))
+  (maybe-delete-qobject (brush widget))
+  (setf (size widget) NIL
+        (brush widget) NIL))
