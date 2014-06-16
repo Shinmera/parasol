74011d48b24c0d92d56471d37528473051e02ec8
Shinmera <shinmera@tymoon.eu>
2014-06-16 23:32:58 +0200

    Added *compositing-mode-list*

--- toolkit.lisp
+++ toolkit.lisp
@@ -90,21 +90,24 @@
          ,@body
          (#_setWorldTransform ,painter ,transform)))))
 
+(defparameter *compositing-mode-list*
+  '(("Normal" 0)
+    ("Erase" 8)
+    ("Lock Alpha" 9)
+    ("Multiply" 13)
+    ("Screen" 14)
+    ("Overlay" 15)
+    ("Darken" 16)
+    ("Lighten" 17)
+    ("Dodge" 18)
+    ("Burn" 19)
+    ("Hard Light" 20)
+    ("Soft Light" 21)
+    ("Difference" 22)
+    ("Exclusion" 23)))
+
 (defparameter *compositing-mode-map*
   (let ((map (make-hash-table :test 'equalp)))
-    (loop for (key val) in '(("Normal" 0)
-                             ("Erase" 8)
-                             ("Lock Alpha" 9)
-                             ("Multiply" 13)
-                             ("Screen" 14)
-                             ("Overlay" 15)
-                             ("Darken" 16)
-                             ("Lighten" 17)
-                             ("Dodge" 18)
-                             ("Burn" 19)
-                             ("Hard Light" 20)
-                             ("Soft Light" 21)
-                             ("Difference" 22)
-                             ("Exclusion" 23))
+    (loop for (key val) in *compositing-mode-list*
           do (setf (gethash key map) val))
     map))
