d3e4079d529a3af72c57dde32fef964ee802b752
Shinmera <shinmera@tymoon.eu>
2014-06-16 21:02:32 +0200

    Added point-opacity

--- brushes/complex-brush.lisp
+++ brushes/complex-brush.lisp
@@ -8,11 +8,14 @@
 (named-readtables:in-readtable :qt)
 
 (defclass complex-brush (brush rotational-brush compositing-brush)
-  ((%brush-size :initarg :brush-size :initform 10 :accessor brush-size))
+  ((%brush-size :initarg :brush-size :initform 10 :accessor brush-size)
+   (%point-opacity :initarg :point-opacity :initform 1.0 :accessor point-opacity))
   (:metaclass brush-class)
-  (:fields (brush-size :type :float :range (0.01 200.0 1.0) :slot %brush-size)))
+  (:fields (brush-size :type :float :range (0.01 200.0 1.0) :slot %brush-size)
+           (point-opacity :type :float :range (0.0 10.0 0.1) :slot %point-opacity)))
 
 (defmethod draw-point ((brush complex-brush) painter x y xt yt p)
   (let ((len (* p (brush-size brush))))
     (with-objects ((point (#_new QPointF 0 0)))
-      (#_drawEllipse painter point (* len (1+ xt)) (* len (1+ yt))))))
+      (#_setOpacity painter (* p (point-opacity brush)))
+      (#_drawEllipse painter point (+ len ) (+ len )))))
