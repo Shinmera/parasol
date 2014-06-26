da9341fb7bd3ffcf1bfb486918bff90442f1e97f
Shinmera <shinmera@tymoon.eu>
2014-06-26 15:59:19 +0200

    base spline curve implemented, not yet spacing points correctly.

--- curves/base.lisp
+++ curves/base.lisp
@@ -7,14 +7,20 @@
 (in-package #:org.tymoonnext.parasol)
 
 (defclass base (curve)
-  ())
+  ((%interpolated :initform (make-array 5 :adjustable T :fill-pointer 0) :accessor interpolated)
+   (%remainder :initform 0 :accessor remainder)))
+
+(defmethod initialize-instance :after ((curve base) &key)
+  (loop repeat (array-dimension (data curve) 0)
+        do (vector-push-extend (make-array 0 :element-type 'float :initial-element 0.0 :adjustable T :fill-pointer T) (data curve))
+           (vector-push-extend (make-array 0 :element-type 'float :initial-element 0.0 :adjustable T :fill-pointer T) (interpolated curve))))
 
 (defmethod (setf point-distance) (point-distance (curve base))
   (setf (slot-value curve '%point-distance)
         point-distance)
   (loop for int across (interpolated curve)
         do (setf (fill-pointer int) 0))
-  (recalculate-linear-interpolations curve))
+  (recalculate-base-spline-interpolations curve))
 
 (defun calculate-base-spline-distances (curve)
   ;; Cheat with linear interpolation
@@ -30,6 +36,40 @@
         (loop for i from (length dist) below (1- len)
               do (vector-push-extend (dist i (1+ i)) dist))))))
 
+(defun recalculate-base-spline-interpolations (curve &key (from 0))
+  (let* ((dist (distances curve))
+         (pointd (point-distance curve))
+         (len (length (aref (data curve) 0))))
+    (when (< 3 len)
+      (loop with i = from
+            with seglen = (aref dist 0)
+            for cdist = (remainder curve)
+              then (+ cdist pointd)
+            do (loop while (>= cdist seglen)
+                     do (incf i)
+                        (setf cdist (- cdist seglen))
+                        (if (< i (1- len))
+                            (setf seglen (elt dist i))
+                            (return)))
+            while (< (+ i 3) len)
+            do (loop for int across (interpolated curve)
+                     for dat across (data curve)
+                     do (let* ((x1 (aref dat i))
+                               (x2 (aref dat (+ i 1)))
+                               (x3 (aref dat (+ i 2)))
+                               (x4 (aref dat (+ i 3)))
+                               (a0 (/ (+ (- x1) (* 3 x2) (- (* 3 x3)) x4) 6))
+                               (a1 (/ (+ (* 3 x1) (- (* 6 x2)) (* 3 x3)) 6))
+                               (a2 (/ (+ (- (* 3 x1)) (* 3 x3)) 6))
+                               (a3 (/ (+ x1 (* 4 x2) x3) 6))
+                               (s (/ cdist seglen)))
+                          (vector-push-extend
+                           (+ a3 (* s (+ a2 (* s (+ a1 (* s a0))))))
+                           int)))
+            finally (setf (remainder curve) cdist)))
+    (setf (point-amount curve)
+          (fill-pointer (aref (interpolated curve) 0)))))
+
 (defmethod make-curve ((type (eql 'base)))
   (make-instance 'base))
 
@@ -38,8 +78,7 @@
         for i from 0
         do (vector-push-extend (float data) (elt (data curve) i)))
   (calculate-base-spline-distances curve)
-  ;;(recalculate-linear-interpolations curve :from (- (length (aref (data curve) 0)) 3))
-  )
+  (recalculate-base-spline-interpolations curve :from (- (length (aref (data curve) 0)) 4)))
 
 (defmethod point-data ((spline base) pos)
   (list (aref (aref (interpolated spline) 0) pos)
