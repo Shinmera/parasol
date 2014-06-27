6a45a0bd5a32aacb66170c3c24dc3f856f742a71
Shinmera <shinmera@tymoon.eu>
2014-06-27 18:28:57 +0200

    Added default methods for document saving (file chooser dialog, class delegator)

--- file-formats/file-format.lisp
+++ file-formats/file-format.lisp
@@ -15,3 +15,20 @@
 
 (defgeneric load-document (file-format document pathname)
   (:documentation ""))
+
+(defmethod save-document ((file-format null) (document document) (pathname null))
+  (let ((path (#_QFileDialog::getSaveFileName *window* "Choose File" (uiop:native-namestring (user-homedir-pathname))))) ;; add proper filters
+    (when (< 0 (length path))
+      (handler-case
+          (progn (save-document nil document (uiop:parse-native-namestring path)) T)
+        (error (err)
+          (#_QMessageBox::critical *window* "Error" (format NIL "Error: ~a" err))
+          NIL)))))
+
+(defmethod save-document ((file-format null) (document document) pathname)
+  (setf (file document) pathname)
+  (let ((type (pathname-type pathname)))
+    (let ((symbol (find-symbol (string-upcase type) "PARASOL")))
+      (if (and symbol (find-class symbol))
+          (save-document (make-instance symbol) document pathname)
+          (error "Unknown file format: ~a" type)))))
