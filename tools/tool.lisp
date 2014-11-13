8241468d1b64d7f4fef91459eda3e4013cfaa826
Shinmera <shinmera@tymoon.eu>
2014-11-13 11:46:52 +0100

    Stubb

--- tools/tool.lisp
+++ tools/tool.lisp
@@ -6,3 +6,19 @@
 
 (in-package #:org.shirakumo.parasol)
 
+;; Metaclass to automatically define
+;; tool options and integrate into
+;; the UI, similar to how I did done
+;; dune it with the brushes?
+(defclass tool ()
+  ())
+
+(defgeneric select (tool))
+
+(defgeneric deselect (tool))
+
+(defgeneric begin (tool pen))
+
+(defgeneric move (tool pen))
+
+(defgeneric end (tool pen))
