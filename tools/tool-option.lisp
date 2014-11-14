6b9289c855e64e4bd73fc5f5b15c389b0f6d2bc8
Shinmera <shinmera@tymoon.eu>
2014-11-14 11:40:18 +0100

    Added DEFINE-TOOL macro

--- tools/tool-option.lisp
+++ tools/tool-option.lisp
@@ -26,3 +26,39 @@ Author: Nicolas Hafner <shinmera@tymoon.eu>
   (define-initializer option 100
     (unless (label option)
       (setf (label option) (capitalize-on #\- (class-name (class-of option)) #\Space T)))))
+
+(defmacro define-tool (name direct-superclasses direct-slots &body options)
+  (destructuring-bind (name &optional (label (capitalize-on #\- name #\Space T))
+                                      (description ""))
+      (if (listp name) name (list name))
+    (unless (find 'tool direct-superclasses)
+      (push 'tool direct-superclasses))
+    (unless (getf options :label)
+      (push (list :label label) options))
+    (unless (getf options :description)
+      (push (list :description description) options))
+    `(defclass ,name ,direct-superclasses
+       ,direct-slots
+       ,@options)))
+
+(defmacro define-tool-option (name (qt-class &rest direct-superclasses) direct-slots &body options)
+  (destructuring-bind (name &optional (label (capitalize-on #\- name #\Space T))
+                                      (description ""))
+      (if (listp name) name (list name))
+    (when (loop for superclass in direct-superclasses
+                never (typep superclass 'tool-option))
+      (push 'tool-option direct-superclasses))
+    (unless (getf options :label)
+      (push (list :label label) options))
+    (unless (getf options :description)
+      (push (list :description description) options))
+    `(define-widget ,name (,qt-class ,direct-superclasses)
+       ,direct-slots
+       ,@options)))
+
+(indent:define-indentation define-tool-option
+    (4 (&whole 6 &rest)
+       (&whole 2 (&whole 0 0 &rest 2))
+       &rest (&whole 2 2 &rest (&whole 2 2 4 &body))))
+
+(declare-environment-widget-form 'define-tool-option)
