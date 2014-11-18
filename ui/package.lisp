19daa82931f949706c20527a562dca8a72e346ee
Shinmera <shinmera@tymoon.eu>
2014-11-18 11:23:26 +0100

    Exports

--- ui/package.lisp
+++ ui/package.lisp
@@ -7,4 +7,40 @@ Author: Nicolas Hafner <shinmera@tymoon.eu>
 (in-package #:org.shirakumo.parasol)
 (defpackage #:parasol.ui
   (:use #:cl #:parasol #:parasol.document #:parasol.tools)
-  (:nicknames #:org.shirakumo.parasol.ui))
+  (:nicknames #:org.shirakumo.parasol.ui)
+  ;; document-view.lisp
+  (:export
+   #:*mouse-pressure*
+   #:document-view
+   #:document
+   #:angle
+   #:zoom
+   #:mirror-x
+   #:mirror-y
+   #:pen
+   #:pen-pressed)
+  ;; gizmo-bar.lisp
+  (:export
+   #:gizmo-bar)
+  ;; main-window.lisp
+  (:export
+   #:*window*
+   #:main-window
+   #:tool
+   #:current-view
+   #:current-document
+   #:main)
+  ;; tab-area.lisp
+  (:export
+   #:tab
+   #:title
+   #:parent
+   #:index
+   
+   #:welcome-tab
+   
+   #:tab-area
+   #:add-tab
+   #:change-tab
+   #:close-tab
+   #:current-tab))
