9af2455f13105fb1a2ee0742ca28b0d8fd5968aa
Shinmera <shinmera@tymoon.eu>
2014-11-22 14:26:13 +0100

    Fixes, added make-target-from, fit gf.

--- document/target.lisp
+++ document/target.lisp
@@ -17,21 +17,84 @@
 (defun make-target (width height)
   (make-instance *target-backend* :width width :height height))
 
+(defun make-target-from (pathname)
+  (let* ((image (#_new QImage (uiop:native-namestring pathname)))
+         (target (make-target (#_width image) (#_height image))))
+    (with-painter (painter target)
+      (#_drawImage painter 0 0 image))
+    (finalize image)
+    target))
+
 (defgeneric to-image (target)
-  (:method to-image ((target target))
+  (:method ((target target))
     (error "TO-IMAGE of the target ~s is not implemented." target)))
 
 (defgeneric get-painter (target)
-  (:method get-painter ((target target))
+  (:method ((target target))
     (error "GET-PAINTER of the target ~s is not implemented." target)))
 
 (defmethod copy ((target target))
   (error "COPY of the target ~s is not implemented." target))
 
 (defgeneric clear (target &optional color)
-  (:method clear ((target target) &optional color)
+  (:method ((target target) &optional color)
     (declare (ignore color))
     (error "CLEAR of the target ~s is not implemented." target)))
+
+(defmethod draw ((target target) painter)
+  (error "DRAW of the target ~s is not implemented." target))
+
+(defgeneric fit (target width height &key x y)
+  (:method ((target target) width height &key x y)
+    (declare (ignore width height x y))
+    (error "FIT of the target ~s is not implemented." target)))
+
+(defun ensure-containable (x y target &key (chunk-size (width target)))
+  (flet ((expand-to (n)
+           (* chunk-size (ceiling (/ (+ (abs n) (/ chunk-size 2)) chunk-size)))))
+    (let ((left (expand-to x))
+          (top (expand-to y)))
+      (cond
+        ;; +-+-+
+        ;; |3|4|
+        ;; +-+-+
+        ;; |2|1|
+        ;; +-+-+
+        ;; Yes this could be written much shorter,
+        ;; but it gets gross to read, so I'll leave
+        ;; it at this variant.
+        ;; 1st Quadrant
+        ((and (< (/ chunk-size 2) x) (< (/ chunk-size 2) y))
+         (values
+          (fit target
+               (max (width target) left)
+               (max (height target) top))
+          0 0))
+        ;; 2nd Quadrant
+        ((and (<= x (/ chunk-size 2)) (< 0 y))
+         (values
+          (fit target
+               (+ left (width target))
+               (max (height target) top)
+               :x left)
+          (- left) 0))
+        ;; 3rd Quadrant
+        ((and (<= x (/ chunk-size 2)) (<= y (/ chunk-size 2)))
+         (values
+          (fit target
+               (+ left (width target))
+               (+ top (height target))
+               :x left :y top)
+          (- left) (- top)))
+        ;; 4th Quadrant
+        ((and (< 0 x) (<= y (/ chunk-size 2)))
+         (values
+          (fit target
+               (max (width target) left)
+               (+ top (height target))
+               :y top)
+          0 (- top)))
+        (T (error "HWAT"))))))
 
 ;; QImage impl.
 (define-finalizable qimage-target (target)
@@ -39,8 +102,9 @@
 
 (defmethod initialize-instance :after ((target qimage-target) &key)
   (unless (slot-boundp target 'image)
-    (setf (image target)
-          (make-image (width target) (height target)))))
+    (let ((image (#_new QImage (width target) (height target) (#_QImage::Format_ARGB32))))
+      (#_fill image (#_Qt::transparent))
+      (setf (image target) image))))
 
 (defmethod to-image ((target qimage-target))
   (image target))
@@ -60,6 +124,16 @@
   (#_drawImage painter (#_new QPointF (x target) (y target)) (image target))
   target)
 
+(defmethod fit ((target qimage-target) width height &key (x 0) (y 0))
+  (unless (and (= (width target) width)
+               (= (height target) height))
+    (let ((new (#_new QImage (width target) (height target) (#_QImage::Format_ARGB32))))
+      (#_fill new (#_Qt::transparent))
+      (with-finalizing ((painter (#_new QPainter new)))
+        (#_drawImage painter x y (image target)))
+      (setf (image target) new)))
+  target)
+
 ;; QGLFrameBufferObject
 (define-finalizable gl-framebuffer-target (target)
   ((sampled-buffer :accessor sampled-buffer :finalized T)
@@ -76,15 +150,17 @@
     (#_create context)
     context))
 
-(defmethod initialize-instance :after ((target gl-framebuffer-target) &key)
+(defun make-sampled-framebuffer (width height &optional (samples 4))
   (#_makeCurrent *gl-context*)
   (let ((format (#_new QGLFramebufferObjectFormat)))
-    (#_setSamples format 4)
+    (#_setSamples format samples)
     (#_setAttachment format (#_QGLFramebufferObject::CombinedDepthStencil))
-    (setf (sampled-buffer target)
-          (#_new QGLFramebufferObject (width target) (height target) format)))
-  (setf (buffer target)
-        (#_new QGLFramebufferObject (width target) (height target))))
+    (#_new QGLFramebufferObject width height format)))
+
+(defmethod initialize-instance :after ((target gl-framebuffer-target) &key)
+  (#_makeCurrent *gl-context*)
+  (setf (sampled-buffer target) (make-sampled-framebuffer (width target) (height target)))
+  (setf (buffer target) (#_new QGLFramebufferObject (width target) (height target))))
 
 (defun blit-framebuffer-target (from to width height)
   (let ((rect (#_new QRect 0 0 width height)))
@@ -106,7 +182,7 @@
     (#_fillRect painter 0 0 (width target) (height target) color))
   target)
 
-(defmethod draw ((target qimage-target) painter)
+(defmethod draw ((target gl-framebuffer-target) painter)
   ;; draw using OpenGL
   (#_beginNativePainting painter)
 
@@ -130,3 +206,15 @@
   
   (#_endNativePainting painter))
   
+(defmethod fit ((target gl-framebuffer-target) width height &key (x 0) (y 0))
+  (unless (and (= (width target) width)
+               (= (height target) height))
+    (let ((new (make-sampled-framebuffer width height))
+          (width (min width (width target)))
+          (height (min height (height target))))
+      (#_QGLFramebufferObject::blitFramebuffer
+       (sampled-buffer target) (#_new QRect 0 0 width height) 
+       new (#_new QRect x y width height))
+      (setf (sampled-buffer target) new))
+    (setf (buffer target) (#_new QGLFramebufferObject width height)))
+  target)
