bb6f6a12351f0a2460f351cab28e83e9e70b8ee1
Shinmera <shinmera@tymoon.eu>
2014-11-25 10:26:20 +0100

    Added move brush

---
+++ tools/view/move-tool.lisp
@@ -0,0 +1,24 @@
+#|
+This file is a part of Parasol
+(c) 2014 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
+Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(in-package #:org.shirakumo.parasol.tools.view)
+(named-readtables:in-readtable :qtools)
+
+(with-widget-environment
+  (define-tool (move-tool "Move" "Move the canvas.") ()
+    ((start-pos :initform NIL :accessor start-pos))))
+
+(defmethod begin ((tool move-tool) pen document)
+  (setf (start-pos tool) (cons (- (x-view pen) (x (current-view)))
+                               (- (y-view pen) (y (current-view))))))
+
+(defmethod move ((tool move-tool) pen document)
+  (v:info :test "POS: ~a,~a"
+          (setf (x (current-view)) (- (x-view pen) (car (start-pos tool))))
+          (setf (y (current-view)) (- (y-view pen) (cdr (start-pos tool))))))
+
+(defmethod end ((tool move-tool) pen document)
+  (setf (start-pos tool) NIL))
---
+++ tools/view/parasol-tools-view.asd
@@ -0,0 +1,17 @@
+#|
+ This file is a part of Parasol
+ (c) 2014 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(in-package #:cl-user)
+(asdf:defsystem parasol-tools-view
+  :name "Parasol-Tools-View"
+  :version "0.0.0"
+  :license "Artistic"
+  :author "Nicolas Hafner <shinmera@tymoon.eu>"
+  :maintainer "Nicolas Hafner <shinmera@tymoon.eu>"
+  :serial T
+  :components ((:file "package")
+               (:file "move-tool"))
+  :depends-on (:parasol :parasol-document :parasol-tools :parasol-ui))
