1281f3512febc3b1f7b963e72cc8cef5e3a10900
Shinmera <shinmera@tymoon.eu>
2014-11-25 15:26:24 +0100

    Made view tools autoloaded

--- load.lisp
+++ load.lisp
@@ -34,7 +34,8 @@
   ;; some kind of system to automate this
   ;; or at least make it hookable
   (ensure-loaded :parasol-ui)
-  (ensure-loaded :parasol-tools-brush))
+  (ensure-loaded :parasol-tools-brush)
+  (ensure-loaded :parasol-tools-view))
 
 (defun start ()
   (test-compatibility)
